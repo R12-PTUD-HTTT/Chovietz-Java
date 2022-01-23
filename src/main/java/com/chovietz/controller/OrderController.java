87b73e8cb61b222b19ae06fe9bdd0ee8316f8ca8
nguyen <tiendatdkdp@gmail.com>
2022-01-23 15:09:51 +0700

    Customer History - Add API

--- src/main/java/com/chovietz/controller/OrderController.java
+++ src/main/java/com/chovietz/controller/OrderController.java
@@ -110,7 +110,7 @@ public class OrderController {
 	@RequestMapping("/customer/{id}")
 	@ResponseBody
 	public ResponseEntity<Map<String, Object>> getAllOrderByCustomer(@PathVariable("id") String id,
-	@RequestParam(required = false) int page,@RequestParam(required = false) int size,@RequestParam(defaultValue = "created_date") String sortBy,@RequestParam(defaultValue = "1") int asc) {
+	@RequestParam(defaultValue = "1") int page,@RequestParam(defaultValue = "1") int size,@RequestParam(defaultValue = "created_date") String sortBy,@RequestParam(defaultValue = "1") int asc) {
 		try {   
 			List<Order> orderlst = new ArrayList<Order>();
 			if(page < 0 || size < 0){
